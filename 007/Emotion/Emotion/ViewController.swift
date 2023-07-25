5244e4c89301c9b253517e355182c5b9aa0980d6
Kim junhwan <tazanboy1@naver.com>
2023-07-25 22:23:47 +0900

    기본 UI 생성

---
+++ 007/Emotion/Emotion/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Emotion
+//
+//  Created by JunHwan Kim on 2023/07/25.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
