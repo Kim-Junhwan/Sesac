aa44250dc6ed7b9094317db318f3c4bb5f83ebde
Kim junhwan <tazanboy1@naver.com>
2023-07-25 22:27:52 +0900

    [feat] 감정 클릭 시 클릭한 횟수 출력 기능 추가

--- 007/Emotion/Emotion/ViewController.swift
+++ 007/Emotion/Emotion/ViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var emotionScoreList: [Int] = Array(repeating: 0, count: Emotion.allCases.count)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func tapEmotion(_ sender: UIButton) {
+        emotionScoreList[sender.tag] += 1
+        if let emotion = Emotion(rawValue: sender.tag)?.currentCase() {
+            print("\(emotion) \(emotionScoreList[sender.tag])")
+        }
+        
     }
     
 }
