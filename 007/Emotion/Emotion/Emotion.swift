33dc0876f4464dc21697858101664d8a2bdcdfaa
Kim junhwan <tazanboy1@naver.com>
2023-07-25 22:25:54 +0900

    Emotion Enum 타입으로 정의

---
+++ 007/Emotion/Emotion/Emotion.swift
@@ -0,0 +1,29 @@
+//
+//  Emotion.swift
+//  Emotion
+//
+//  Created by JunHwan Kim on 2023/07/25.
+//
+
+enum Emotion: Int, CaseIterable {
+    case veryGood = 0
+    case good
+    case soso
+    case sad
+    case verySad
+    
+    func currentCase() -> String {
+        switch self {
+        case .veryGood:
+            return "veryGood"
+        case .good:
+            return "good"
+        case .soso:
+            return "soso"
+        case .sad:
+            return "sad"
+        case .verySad:
+            return "verySad"
+        }
+    }
+}
