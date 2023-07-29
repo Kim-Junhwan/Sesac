c1cfcf4686fc8fcb80414ab4edf15a2209d1b790
Kim junhwan <tazanboy1@naver.com>
2023-07-29 10:03:57 +0900

    [feat] 영화테이블뷰 셀 정의 및 configure 함수 생성

--- 010/CustomCellTableView/CustomCellTableView/Movie/MovieTableViewCell.swift
+++ 010/CustomCellTableView/CustomCellTableView/Movie/MovieTableViewCell.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    func configureCell(movie: Movie) {
+        posterImageView.image = UIImage(named: movie.title)
+        movieNameLabel.text = movie.title
+        releaseDateLabel.text = movie.releaseDate
+        runtimeLabel.text = String(movie.runtime)
+        scoreLabel.text = String(movie.rate)
+        overviewLabel.text = movie.overview
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
